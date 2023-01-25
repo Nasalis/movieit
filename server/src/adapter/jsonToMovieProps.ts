8b00b563548e227016b19431714f62967a086b8b
Davi dos Santos Freitas <davidossantosfreitas19@gmail.com>
2023-01-25 20:27:45 -0300

    feat: add simple adapter

---
+++ server/src/adapter/jsonToMovieProps.ts
@@ -0,0 +1,24 @@
+import { IMovieProps } from '../app/entities/Movie';
+
+export class JsonToMovieProps {
+  public static toMovieProps(jsonData: any): IMovieProps {
+    return {
+      id: jsonData.id,
+      backdrop_path: jsonData.backdrop_path,
+      budget: jsonData.budget,
+      genres: jsonData.genres,
+      homepage: jsonData.homepage,
+      original_language: jsonData.original_language,
+      original_title: jsonData.original_title,
+      overview: jsonData.overview,
+      poster_path: jsonData.poster_path,
+      production_companies: jsonData.production_companies,
+      release_date: jsonData.release_date,
+      revenue: jsonData.revenue,
+      runtime: jsonData.runtime,
+      status: jsonData.status,
+      tagline: jsonData.tagline,
+      title: jsonData.title,
+    };
+  }
+}
