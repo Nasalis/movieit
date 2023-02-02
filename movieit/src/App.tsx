43e99934d74613f510cf6c7ae35cb4de43980913
Davi dos Santos Freitas <davidossantosfreitas19@gmail.com>
2023-02-01 23:46:40 -0300

    feat: add initial version to home

--- movieit/src/App.tsx
+++ movieit/src/App.tsx
@@ -1,32 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Logo } from "./components/utils/Logo"
+import { Compass, Heart, MagnifyingGlass, MonitorPlay } from "phosphor-react";
+import { TrendingMovieCard } from "./components/TrendingMovieCard";
+import { ContentCard } from "./components/ContentCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const sideBarOptions = [
+    {
+      icon: <Compass size={20} weight="fill" className="text-red-100 mr-1"/>,
+      label: "Browse"
+    },
+    {
+      icon: <MonitorPlay size={20} weight="fill" className="text-red-100 mr-1"/>,
+      label: "Watchlist", 
+    },
+    {
+      icon: <Heart size={20} weight="fill" className="text-red-100 mr-1"/>,
+      label: "Favorites"
+    }
+  ]
+
+  const navigationOptions = ["Home", "Movies", "TV shows/Series"]
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="grid grid-cols-4 min-h-screen max-h-full bg-black-200">
+      <aside className="col-start-1 col-end-2 min-w-full h-full border-r-2 border-r-black-400">
+        <div className=" p-4">
+          <Logo/>
+          <div className="w-full h-[2px] bg-black-400 mt-4 mb-4"></div>
+        </div>
+        <ul>
+          {sideBarOptions.map(option => (
+            <li className="group flex items-center pl-4 pr-4 mb-4 text-white font-bold relative group">
+              <div className="w-0.5 h-5 bg-transparent absolute left-0 group-hover:bg-red-100 ease-in duration-100 cursor-pointer"></div>
+              {option.icon}
+              {option.label}
+            </li>
+          ))}
+        </ul>
+      </aside>
+      <main className="grid col-start-2 col-end-5 w-full max-h-screen pl-8 py-4 gap-y-9 overflow-y-auto">
+        <div className="relative max-w-md">
+          <MagnifyingGlass size={20} weight="fill" className="absolute top-2 left-3 text-gray-300" />
+          <input className="w-full p-2 pl-9 text-white text-xs font-semibold rounded-2xl border-gray-300 border-x-2 border-y-2 bg-transparent" type="text" placeholder="Search everything" min={2} />
+        </div>
+        <nav className="max-w-[15.625rem]">
+          <ul className="flex items-center justify-between gap-4">
+            {navigationOptions.map(el => (
+              <li className="text-white text-base font-bold">
+                {el}
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <section className="flex flex-col items-start justify-center gap-4">
+          <h2 className="text-white text-4xl font-bold capitalize">
+            Trending movies
+          </h2>
+          <TrendingMovieCard 
+            backgroundPath="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/wgq2oA1aJiawBlRDIU8agXk7DQQ.jpg"
+            posterPath="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lLeWJU33HgfWTnrOcaNBR5fUcF1.jpg"
+          />
+        </section>
+        <section className="flex flex-col items-start justify-center gap-4">
+          <h2 className="text-white text-4xl font-bold capitalize">
+            Top rated
+          </h2>
+          <ul className="flex items-center justify-start w-full">
+            <li className="mr-8">
+              <ContentCard/>
+            </li>
+            <li className="mr-8">
+              <ContentCard/>
+            </li>
+            <li className="mr-8">
+              <ContentCard/>
+            </li>
+          </ul>
+        </section>
+      </main>
     </div>
   )
 }
