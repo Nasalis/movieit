7dfe9c3bd9489cf20798b7d3d1a16add2941ba75
Davi dos Santos Freitas <davidossantosfreitas19@gmail.com>
2023-02-01 23:46:07 -0300

    feat: create reception icon component

---
+++ movieit/src/components/ReceptionIcon.tsx
@@ -0,0 +1,13 @@
+interface ReceptionIconProps {
+  symbol: string;
+}
+
+export function ReceptionIcon({ symbol }: ReceptionIconProps) {
+  return (
+    <div className="w-full h-full flex justify-center">
+      <b className="w-8 h-8 text-white font-bold flex items-center justify-center rounded-md bg-yellow-100">
+        {symbol}
+      </b>
+    </div>
+  )
+}
