2e7cba2d8856b2807fc51c5578ff20ae030a138b
Davi dos Santos Freitas <davidossantosfreitas19@gmail.com>
2023-02-06 00:07:32 -0300

    feat: add initial version to home page

--- movieit/src/App.tsx
+++ movieit/src/App.tsx
@@ -2,6 +2,7 @@ import { Logo } from "./components/utils/Logo"
 import { Compass, Heart, MagnifyingGlass, MonitorPlay } from "phosphor-react";
 import { TrendingMovieCard } from "./components/TrendingMovieCard";
 import { ContentCard } from "./components/ContentCard";
+import { Slider } from "./components/Slider";
 
 function App() {
 
@@ -22,6 +23,29 @@ function App() {
 
   const navigationOptions = ["Home", "Movies", "TV shows/Series"]
 
+  const movies = [
+    {
+      backgroundPath: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/wgq2oA1aJiawBlRDIU8agXk7DQQ.jpg",
+      posterPath: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lLeWJU33HgfWTnrOcaNBR5fUcF1.jpg"
+    },
+    {
+      backgroundPath: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/wgq2oA1aJiawBlRDIU8agXk7DQQ.jpg",
+      posterPath: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lLeWJU33HgfWTnrOcaNBR5fUcF1.jpg"
+    },
+    {
+      backgroundPath: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/wgq2oA1aJiawBlRDIU8agXk7DQQ.jpg",
+      posterPath: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lLeWJU33HgfWTnrOcaNBR5fUcF1.jpg"
+    },
+    {
+      backgroundPath: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/wgq2oA1aJiawBlRDIU8agXk7DQQ.jpg",
+      posterPath: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lLeWJU33HgfWTnrOcaNBR5fUcF1.jpg"
+    },
+    {
+      backgroundPath: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/wgq2oA1aJiawBlRDIU8agXk7DQQ.jpg",
+      posterPath: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lLeWJU33HgfWTnrOcaNBR5fUcF1.jpg"
+    }
+  ]
+
   return (
     <div className="grid grid-cols-4 min-h-screen max-h-full bg-black-200">
       <aside className="col-start-1 col-end-2 min-w-full h-full border-r-2 border-r-black-400">
@@ -39,7 +63,9 @@ function App() {
           ))}
         </ul>
       </aside>
-      <main className="grid col-start-2 col-end-5 w-full max-h-screen pl-8 py-4 gap-y-9 overflow-y-auto">
+      <main  
+        className="grid col-start-2 col-end-5 w-full max-h-screen pl-8 py-4 gap-y-9 overflow-y-auto overflow-x-hidden"
+      >
         <div className="relative max-w-md">
           <MagnifyingGlass size={20} weight="fill" className="absolute top-2 left-3 text-gray-300" />
           <input className="w-full p-2 pl-9 text-white text-xs font-semibold rounded-2xl border-gray-300 border-x-2 border-y-2 bg-transparent" type="text" placeholder="Search everything" min={2} />
@@ -53,20 +79,47 @@ function App() {
             ))}
           </ul>
         </nav>
-        <section className="flex flex-col items-start justify-center gap-4">
+        <section className="relative flex flex-col items-start justify-center gap-4 overflow-x-hidden h-96">
           <h2 className="text-white text-4xl font-bold capitalize">
             Trending movies
           </h2>
-          <TrendingMovieCard 
-            backgroundPath="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/wgq2oA1aJiawBlRDIU8agXk7DQQ.jpg"
-            posterPath="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lLeWJU33HgfWTnrOcaNBR5fUcF1.jpg"
-          />
+          <Slider>
+            <TrendingMovieCard 
+              backgroundPath="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/wgq2oA1aJiawBlRDIU8agXk7DQQ.jpg"
+              posterPath="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lLeWJU33HgfWTnrOcaNBR5fUcF1.jpg"
+            />
+            <TrendingMovieCard 
+              backgroundPath="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/wgq2oA1aJiawBlRDIU8agXk7DQQ.jpg"
+              posterPath="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lLeWJU33HgfWTnrOcaNBR5fUcF1.jpg"
+            />
+            <TrendingMovieCard 
+              backgroundPath="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/wgq2oA1aJiawBlRDIU8agXk7DQQ.jpg"
+              posterPath="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lLeWJU33HgfWTnrOcaNBR5fUcF1.jpg"
+            />
+            <TrendingMovieCard 
+              backgroundPath="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/wgq2oA1aJiawBlRDIU8agXk7DQQ.jpg"
+              posterPath="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lLeWJU33HgfWTnrOcaNBR5fUcF1.jpg"
+            />
+            <TrendingMovieCard 
+              backgroundPath="https://www.themoviedb.org/t/p/w533_and_h300_bestv2/wgq2oA1aJiawBlRDIU8agXk7DQQ.jpg"
+              posterPath="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lLeWJU33HgfWTnrOcaNBR5fUcF1.jpg"
+            />
+          </Slider>
         </section>
         <section className="flex flex-col items-start justify-center gap-4">
           <h2 className="text-white text-4xl font-bold capitalize">
             Top rated
           </h2>
-          <ul className="flex items-center justify-start w-full">
+          <ul className="flex flex-wrap items-center gap-10 justify-center w-full">
+            <li className="mr-8">
+              <ContentCard/>
+            </li>
+            <li className="mr-8">
+              <ContentCard/>
+            </li>
+            <li className="mr-8">
+              <ContentCard/>
+            </li>
             <li className="mr-8">
               <ContentCard/>
             </li>
@@ -78,7 +131,7 @@ function App() {
             </li>
           </ul>
         </section>
-      </main>
+      </main >
     </div>
   )
 }
