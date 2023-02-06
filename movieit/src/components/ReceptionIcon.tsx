edbab5e9888a7bfecce683da8532095ae0e45516
Davi dos Santos Freitas <davidossantosfreitas19@gmail.com>
2023-02-06 00:09:00 -0300

    feat: fix content position

--- movieit/src/components/ReceptionIcon.tsx
+++ movieit/src/components/ReceptionIcon.tsx
@@ -4,7 +4,7 @@ interface ReceptionIconProps {
 
 export function ReceptionIcon({ symbol }: ReceptionIconProps) {
   return (
-    <div className="w-full h-full flex justify-center">
+    <div className="w-full h-full flex justify-end">
       <b className="w-8 h-8 text-white font-bold flex items-center justify-center rounded-md bg-yellow-100">
         {symbol}
       </b>
