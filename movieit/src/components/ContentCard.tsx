6e368943a2695e59159d3ea147227a0a791417e9
Davi dos Santos Freitas <davidossantosfreitas19@gmail.com>
2023-02-06 00:07:01 -0300

    feat: update style in card

--- movieit/src/components/ContentCard.tsx
+++ movieit/src/components/ContentCard.tsx
@@ -32,11 +32,11 @@ export function ContentCard() {
         <div className="absolute left-0 top-0 w-full h-full rounded-t-2xl bg-gradient-to-t from-black-100 to-transparent" ></div>
         <img className="rounded-t-2xl " src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/sWgBv7LV2PRoQgkxwlibdGXKz1S.jpg" alt="" />
       </div>
-      <footer className="grid grid-cols-2 gap-y-2 grid-rows-2 p-2 bg-blue-100">
+      <footer className="grid grid-cols-2 gap-y-2 grid-rows-2 items-center p-2 bg-blue-100">
         <span className="col-start-1 col-end-3 row-start-1 row-end-1 text-white text-base font-bold">
           The Mandalorian
         </span>
-        <small className="col-start-1 col-end-1 row-start-2 row-end-2 text-gray-200 text-xs">
+        <small className="col-start-1 col-end-3 row-start-2 row-end-2 text-gray-200 text-xs">
           Sci-fi / Adventure / Action
         </small>
         <span className="col-start-2 col-end-3 row-start-2 row-end-2">
